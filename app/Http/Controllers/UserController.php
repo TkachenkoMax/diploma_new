720df13c2d4d75a2d6561387628ffb2a80aee1fc
Maxim Tkachenko <maxim.tkachenko@sigma.software>
2018-05-20 15:29:21 +0300

    Log delete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,6 @@ use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 
 /**
  * Class UserController
